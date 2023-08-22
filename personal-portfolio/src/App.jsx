6e0385dfd12a4323b00c525a1bdf40854de3e6f0
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-08-22 16:54:24 +0100

    Stylin =g toggleFunctionality

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -20,9 +20,9 @@ function Header() {
 
   return (
     <header>
-      <div className="profile-card">
-        <div className="profile-card-info">
-          <img src="profile-pic.jpg" alt="profile-img" />
+      <div className="profile_card">
+        <div className="profile_card_info">
+          <img src="profile-pic.jpg" alt="profile_Img" />
 
           <div>
             <h3> {userName} </h3>
@@ -30,8 +30,8 @@ function Header() {
           </div>
         </div>
 
-        <div className="resume-btn">
-          <a href="/resume" className="resume-btn-link">Download Resume</a>
+        <div className="resume_btn">
+          <a href="/resume" className="resume_btn_link">Download Resume</a>
         </div>
       </div>
     </header>
@@ -39,13 +39,32 @@ function Header() {
 }
 
 function Navigation() {
+  const [navigation, setNavigation] = useState("");
+
+  function handleNavigation() {
+    // e.innerHTML();
+
+    !navigation ? setNavigation(() => "btn_navigation_active") : setNavigation("");
+    // setNavigation(navigation => !navigation);
+  }
+
+  console.log(navigation);
+
   return (
-    <div className="navigation-list-section">
-      <ul className="navigation-list">
-        <button><li className="navigation-active" id="#About">About</li></button>
-        <button><li className="active" id="#Showcase">Showcase</li></button>
-        <button><li className="active" id="#Contact">Contact</li></button>
-        <button><li className="active" id="#Blog">Blog</li></button>
+    <div className="navigation_list_section">
+      <ul className="navigation_list">
+        <button key="about" onClick={handleNavigation} className= {navigation ? "btn_navigation_active" : ""} id="#About">
+          <li className="navigation_active_list" id="#About">About</li>
+        </button>
+        <button key="showcase" onClick={handleNavigation} className={navigation ? "btn_navigation_active" : ""} id="#Showcase">
+          <li className="active" id="#Showcase">Showcase</li>
+        </button>
+        <button key="contact" onClick={handleNavigation} className={navigation ? "btn_navigation_active" : ""} id="#Contact">
+          <li className="active" id="#Contact">Contact</li>
+        </button>
+        <button key="blog" onClick={handleNavigation} className={navigation ? "btn_navigation_active" : ""} id="#Blog">
+          <li className="active" id="#Blog">Blog</li>
+        </button>
       </ul>
     </div>
   );
