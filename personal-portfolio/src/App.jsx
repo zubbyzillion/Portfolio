ddfee57a7521225fe2d8fcb2e43bcc6aa7fe0b5f
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-09-21 19:33:59 +0100

    Adding Description

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -7,8 +7,10 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Navigation />
+    <main className="container">
+        <Header />
+        <Navigation />
+      </main>
     </>
   );
 }
@@ -22,7 +24,8 @@ function Header() {
     <header>
       <div className="profile_card">
         <div className="profile_card_info">
-          <img src="profile-pic.jpg" alt="profile_Img" />
+          <span className="img" aria-label="Photo of Nzubechukwu" role="profile_img"></span>
+          {/* <img src="profile-pic.jpg" alt="profile_Img" /> */}
 
           <div>
             <h3> {userName} </h3>
@@ -51,33 +54,50 @@ function Navigation() {
 
   return (
     <>
-    <section className="navigation_list_section">
-      <div className="navigation_list">
-        <div className="navigation_data_list_w">
-          {Children.toArray(
-            navigationPages.map((nav) => {
-                return (
-                  <div key={nav}>
-                    <input type="radio" id={nav} name="nav" onChange={() => setCurrentNavigation(nav)} />
-                    <label htmlFor={nav} className={currentNavigation === nav ? "active" : null}> {nav} </label>
-                  </div>
-                )
-              })
-          )}
+      <section className="navigation_list_section">
+        <div className="navigation_list">
+          <div className="navigation_data_list_w">
+            {Children.toArray(
+              navigationPages.map((nav) => {
+                  return (
+                    <div key={nav}>
+                      <input type="radio" id={nav} name="nav" onChange={() => setCurrentNavigation(nav)} />
+                      <label htmlFor={nav} className={currentNavigation === nav ? "active" : null}> {nav} </label>
+                    </div>
+                  )
+                })
+            )}
+          </div>
         </div>
-      </div>
-    </section>
-    
-    <section className="navigation_list_drawer">
-      <section className="navigation_list_container">{renderedPages[currentNavigation]}</section>
-    </section>
+      </section>
+      
+      <section className="navigation_list_drawer">
+        <section className="navigation_list_container">{renderedPages[currentNavigation]}</section>
+      </section>
     </>
   );
 }
 
 function About()  {
   return (
-    <div className="about_container">About</div>
+    <div className="about_container">
+      <h3 className="about_heading">Hello there! 👋, </h3>
+
+      <br />
+
+      <p className="about_text">&nbsp; &nbsp; My name is Nzubechukwu P. Beluonwu-Ogbo and I am a UX Engineer, <br /> 
+        A lifelong learner eager to try out new things to drive business growth and creating amazing experience for users.
+      </p>
+
+      <br />
+      <br />
+
+      <div className="xP_detail">
+        <p className="xP_text">
+        &nbsp; &nbsp; I have gained over 3+ years of experience and proficiency working with various front-end/web frameworks building top-level Web Apps and features.
+        </p>
+      </div>
+    </div>
   );
 }
 
